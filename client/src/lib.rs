a66e79c0f54db6143947e7fc2bc546de846d0796
Mubelotix <unbeliever.in.time@gmail.com>
2019-12-06 18:40:53 +0100

    client: drawing improvements

--- client/src/lib.rs
+++ client/src/lib.rs
@@ -32,6 +32,15 @@ macro_rules! println {
     ($($t:tt)*) => (log(&format_args!($($t)*).to_string()))
 }
 
+fn map_coords_to_screen_coords((x, y): (u64, u64), (cx, cy): (u64, u64), (width, height): (u32, u32)) -> (u32, u32) {
+    /*println!("{} {}", x, y);
+    println!("{} {}", cx, cy);
+    println!("{} {}", x as i128 - cx as i128, y as i128 - cy as i128);
+    println!("{} {}", (x as i128 - cx as i128) * 40, (y as i128 - cy as i128) * 40);
+    panic!("{} {}", (x as i128 - cx as i128) as u32 * 40 + width / 2, (y as i128 - cy as i128) as u32 * 40 + height / 2);*/
+    ((x - cx) as u32 * 40 + width / 2, (y - cy) as u32 * 40 + height / 2)
+}
+
 fn main(images: Vec<Image>, websocket: Rc<WebSocket>) {
     let mut canvas = Canvas::new();
     // TODO Smooth
@@ -55,7 +64,7 @@ fn main(images: Vec<Image>, websocket: Rc<WebSocket>) {
         .unwrap();
     message.forget();
 
-    websocket.send_with_str(&Message::Init{username: String::new(), screen_width: canvas.get_size().0, screen_height: canvas.get_size().1, password: None}.encode()).expect("can't send init message");
+    websocket.send_with_str(&Message::Init{username: String::from("Mubelotix"), screen_width: canvas.get_size().0, screen_height: canvas.get_size().1, password: None}.encode()).expect("can't send init message");
 
     let mut player: Option<Entity> = None;
     let mut chunks: Vec<Chunk> = Vec::new();
@@ -82,20 +91,24 @@ fn main(images: Vec<Image>, websocket: Rc<WebSocket>) {
             };
         }
 
-        // draw chunks
+        // draw
         canvas.clear();
         if let Some(player) = &player {
             let (x, y) = player.get_coords();
             for chunk in &chunks {
                 
-                let x = (chunk.x - x) as usize + (canvas.get_size().0 / 2) as usize;
-                let y = (chunk.y - y) as usize + (canvas.get_size().1 / 2) as usize;
+                let x = (chunk.x - x) as usize * 40 + ((canvas.get_size().0 - canvas.get_size().0%40) / 2) as usize;
+                let y = (chunk.y - y) as usize * 40 + ((canvas.get_size().1 - canvas.get_size().1%40) / 2) as usize;
                 
                 for i in 0..8 {
                     for j in 0..8 {
                         match chunk.blocks[i][j].get_block_code() {
                             BlockCode::SimpleSlab => {
-                                canvas.draw_image_with_size((x + i * 40) as f64, (y + j * 40) as f64, 40.0, 40.0, &images[0])
+                                //let (x, y) = player.get_coords();
+                                //let coords = map_coords_to_screen_coords((chunk.x + i as u64, chunk.y + j as u64), (x as u64, y as u64), (canvas.get_size().0, canvas.get_size().1));
+                                canvas.draw_image_with_size((x + i * 40) as f64, (y + j * 40) as f64, 40.0, 40.0, &images[0]);
+                                //canvas.draw_image_with_size(coords.0 as f64, coords.1 as f64, 40.0, 40.0, &images[0])
+                                //canvas.fill_rect(((x + i * 40) as f64, (y + j * 40) as f64), (40.0, 40.0));
                             },
                             BlockCode::SimpleWall => {
                                 canvas.draw_image_with_size((x + i * 40) as f64, (y + j * 40) as f64, 40.0, 40.0, &images[1])
@@ -104,6 +117,8 @@ fn main(images: Vec<Image>, websocket: Rc<WebSocket>) {
                     }
                 }
             }
+
+            canvas.draw_image_with_size(((canvas.get_size().0 - canvas.get_size().0%40) / 2) as f64, ((canvas.get_size().1 - canvas.get_size().1%40) / 2) as f64, 40.0, 40.0, &images[2])
         }
         
 
@@ -140,7 +155,7 @@ fn setup_websocket(images: Vec<Image>) {
 pub fn start() -> Result<(), JsValue> {
     panic::set_hook(Box::new(console_error_panic_hook::hook));
     println!("Loading textures...");
-    Image::load_images(vec!["https://cdnb.artstation.com/p/assets/images/images/001/146/571/large/ulrick-wery-tileableset2-groundslab.jpg?1441028618", "https://3docean.img.customer.envatousercontent.com/files/45172611/main1_590.JPG?auto=compress%2Cformat&fit=crop&crop=top&w=590&h=590&s=a4ea7fe36777345d561d83dc79e55874"], setup_websocket);
+    Image::load_images(vec!["https://cdnb.artstation.com/p/assets/images/images/001/146/571/large/ulrick-wery-tileableset2-groundslab.jpg?1441028618", "https://3docean.img.customer.envatousercontent.com/files/45172611/main1_590.JPG?auto=compress%2Cformat&fit=crop&crop=top&w=590&h=590&s=a4ea7fe36777345d561d83dc79e55874", "https://p7.hiclipart.com/preview/924/660/881/2d-computer-graphics-video-game-character-concept-art-2d-game-character-sprites.jpg"], setup_websocket);
 
     Ok(())
 }
